afe7f3baa6b3055e7921708844410b22ef6b5fad
Sunil Prajapati <sunilpzpt@gmail.com>
2023-11-29 18:30:34 +0700

    fix express server

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
-import express from 'express'
-import serveStatic from 'serve-static'
-import path from 'path'
+import express from 'express';
+import serveStatic from 'serve-static';
+import path, { fileURLToPath } from 'path';
 
-const app = express()
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-app.use('/', serveStatic(path.join(__dirname, '/dist')))
+const app = express();
+
+app.use('/', serveStatic(path.join(__dirname, '/dist')));
 
 app.get(/.*/, function (req, res) {
-	res.sendFile(path.join(__dirname, '/dist/index.html'))
-})
+  res.sendFile(path.join(__dirname, '/dist/index.html'));
+});
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8080;
 
 app.listen(port, '0.0.0.0', function () {
-    console.log('Vue app strated!')
-})
+  console.log('Vue app started!');
+});
