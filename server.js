384bbcbe48e4e699bcb8794b2f9589ecfc128e46
Sunil Prajapati <sunilpzpt@gmail.com>
2023-11-29 18:26:37 +0700

    fix express server

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const serveStatic = require('serve-static')
-const path = require('path')
+import express from 'express'
+import serveStatic from 'serve-static'
+import path from 'path'
 
 const app = express()
 
